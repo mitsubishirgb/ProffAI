d39fe32f9036011fa10177bce8d5b9d575e0ce15
mitsubishirgb <bananamilklulw@gmail.com>
2026-01-26 17:22:34 +0100

    Rregullime te vogla

--- public/components/navbar.php
+++ public/components/navbar.php
@@ -4,9 +4,8 @@
             <a href="/index.php">ProffAI</a>  
         </div>
         <ul class="nav-links">
-            <li><a href="../index.php">Home</a></li>  
             <li><a href="../pages/pricing.html">Upgrade</a></li>
-            <li><a href="../pages/about-us.html">About Us</a></li>
+            <li><a href="../pages/about-us.php">About Us</a></li>
             <li><a href="../pages/login.php">Login</a></li>
         </ul>
     </nav>
